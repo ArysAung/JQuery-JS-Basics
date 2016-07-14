d84abe682c81aae12d7b55798ffbf7c08518e930
Arys Aung <arys.aung@gmail.com>
2016-07-14 14:32:51 -0400

    initial + final commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var x = 5
+
+if(x>5){
+	console.log("Booyah!")
+};
+
+if(x<5){
+	console.log("Close but no Cigar")
+};
+
+if (x === 5){
+	console.log("Yoooo!")
+};
+
+var y = 10
+var z = 11
+
+console.log(y + z);
+
+var n1 = "Jack"
+var n2 = "Dough"
+
+console.log(n1 + " " + n2);
+
+var hobby = [["walking","skiing","eating"],["swimming", "biking", "diving"]];
+console.log(hobby.join("\n"));
+
+var a = 9
+
+if(a>10){
+	alert("Your variable is greater than 10!")
+}else{
+	console.log(a + " " + "is less than 10")
+}
+
+var string1 = "swimmingly"
+var string2 = "swimmingly"
+if(string1 !== string2){
+	alert("Your strings are incompatible")
+}else{
+	console.log("Your strings" + " " + string1 + " " + string2 + " " + "are the same.")
+}
+
